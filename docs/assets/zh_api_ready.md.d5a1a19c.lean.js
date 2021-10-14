029e518f141de528992a46721b3aedb06f4812ad
huyanhai <810839700@qq.com>
2021-10-14 09:56:33 +0800

    chore: text website

--- docs/assets/zh_api_ready.md.d5a1a19c.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as n,a as s,g as t}from"./app.b8baa789.js";const p='{"title":"xxx.ready","description":"","frontmatter":{},"headers":[{"level":2,"title":"参数","slug":"参数"}],"relativePath":"zh/api/ready.md","lastUpdated":1633937712457}',e={},o=[t('',8)];var c=a(e,[["render",function(a,t,p,e,c,l){return s(),n("div",null,o)}]]);export{p as __pageData,c as default};
