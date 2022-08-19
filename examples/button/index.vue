1b6c0a2142d56e720e05b3ce3acf142c76d0f4ce
huyanhai <huyanhai@users.noreply.github.com>
2022-08-19 02:56:30 +0000

    deploy: b165a52479fdec65574b4181151612eaae3ea457

---
+++ examples/button/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-row style="margin-bottom: 10px">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+
+  <el-row style="margin-bottom: 10px">
+    <el-button plain>Plain</el-button>
+    <el-button type="primary" plain>Primary</el-button>
+    <el-button type="success" plain>Success</el-button>
+    <el-button type="info" plain>Info</el-button>
+    <el-button type="warning" plain>Warning</el-button>
+    <el-button type="danger" plain>Danger</el-button>
+  </el-row>
+
+  <el-row style="margin-bottom: 10px">
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
+  </el-row>
+</template>
+
+<script lang="ts" setup></script>
